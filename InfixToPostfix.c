d8ad85ace8c161b0ddb29cf08f02c3e377579b92
Manish dhungana <91243906+manishdhungana111@users.noreply.github.com>
2022-09-18 12:17:08 +0545

    database

--- InfixToPostfix.c
+++ InfixToPostfix.c
@@ -50,3 +50,57 @@ int main()
 	return 0;
 	
 }
+create database ecommerece_db;
+
+use ecommerece_db;
+
+
+CREATE TABLE customer(
+ID int not null auto_increment,
+name varchar(45) not null,
+email varchar(45),
+address varchar(45),
+primary key (ID)
+);
+
+CREATE TABLE product(
+id int not null auto_increment,
+name varchar(45) not null,
+price DECIMAL(5,2),
+primary key(id)
+);
+SELECT * FROM product;
+
+INSERT INTO customer (`name`,`email`,`address`)
+VALUES 
+("Manish Dhungana", "manishdhungana@gmail.com", "kathmandu"),
+("Manish Dhungana1", "manishdhungana@gmail.com", "kathmandu"),
+("Manish Dhungana2", "manishdhungana@gmail.com", "kathmandu");
+
+SELECT * FROM customer
+
+INSERT INTO product(`name`,`price`)
+VALUES
+("Keyboard","600.50"),
+("Mouse","450.67");
+
+
+CREATE TABLE orders(
+ID int not null auto_increment,
+customer_id int not null,
+product_id int not null,
+foreign key(customer_id) references customer(ID),
+foreign key(product_id) references product(id),
+primary key(ID)
+);
+
+INSERT into orders(`customer_id`,`product_id`)
+VALUES(2,2);
+
+SELECT orders.ID,orders.customer_id, customer.name, orders.product_id, product.name
+FROM((orders
+INNER JOIN customer on orders.customer_id=customer.ID)
+INNER JOIN product on orders.product_id=product.id) 
+
+SELECT * FROM ecommerece_db.orders
+
